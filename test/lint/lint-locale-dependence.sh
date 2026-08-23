ca6956080bb6e81daf2a4e02179237261e93e963
digirayc <kargain.org@gmail.com>
2026-08-23 09:38:48 +0200

    wallet: drop duplicated includes in txrecord.cpp

--- test/lint/lint-locale-dependence.sh
+++ test/lint/lint-locale-dependence.sh
@@ -56,6 +56,7 @@ KNOWN_VIOLATIONS=(
     "src/util/strencodings.cpp:.*strtoul"
     "src/util/strencodings.h:.*atoi"
     "src/util/system.cpp:.*atoi"
+    "src/wallet/txrecord.cpp:.*to_string"
 )
 
 REGEXP_IGNORE_EXTERNAL_DEPENDENCIES="^src/(crypto/blake3/|crypto/ctaes/|leveldb/|libmw/|secp256k1/|secp256k1-zkp/|tinyformat.h|univalue/)"
