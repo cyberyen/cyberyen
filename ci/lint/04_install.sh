79bea4910dcdf71f1a15cbd307ee17456b677ed8
digirayc <kargain.org@gmail.com>
2026-08-23 10:03:23 +0200

    ci: use a mypy version that installs on Ubuntu 22.04

--- ci/lint/04_install.sh
+++ ci/lint/04_install.sh
@@ -20,14 +20,10 @@ travis_retry() {
   done
 }
 
-travis_retry sudo apt update && sudo apt install -y clang-format-9
-sudo update-alternatives --install /usr/bin/clang-format      clang-format      $(which clang-format-9     ) 100
-sudo update-alternatives --install /usr/bin/clang-format-diff clang-format-diff $(which clang-format-diff-9) 100
-
 travis_retry pip3 install codespell==1.17.1
 travis_retry pip3 install flake8==3.8.3
 travis_retry pip3 install yq
-travis_retry pip3 install mypy==0.781
+travis_retry pip3 install mypy==0.971
 
 SHELLCHECK_VERSION=v0.7.1
 curl -sL "https://github.com/koalaman/shellcheck/releases/download/${SHELLCHECK_VERSION}/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | tar --xz -xf - --directory /tmp/
